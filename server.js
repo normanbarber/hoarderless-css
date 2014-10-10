c9cea96792d80efa60533f3d3b56a886d2d35292
normanbarber <normanebarber@gmail.com>
2014-10-10 17:11:56 -0400

    updating express to > 4

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-var express = require('express'),
-  routes = require('./config/routeProviders'),
-  routesconfig = require('./config/routes');
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var routes = require('./config/routeProviders');
+var routesconfig = require('./config/routes');
 
-var app = module.exports = express();
+var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 8000);
-  app.set('views', __dirname + '/public/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.static(__dirname + '/public'));
-  app.use(app.router);
-});
+app.set('port', process.env.PORT || 8000);
+app.set('views', path.join(__dirname, '/public/views'));
+app.set('view engine', 'jade');
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer());
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 routes.setup({
   app: app,
